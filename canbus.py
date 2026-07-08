67caa662441c0d2eef8fe3039b02ce9df705e57e
dan28076626 <dan28076626@gmail.com>
2026-07-08 18:26:27 +0000

    Added module ids to canbus

--- canbus.py
+++ canbus.py
@@ -1,11 +1,21 @@
 from datetime import datetime as dt
 
 class CANBus:
-    def __init__(self):
-        self.messages = []
+    id_codes={
+            "ECU": "0x101",
+            "DASHBOARD": "0x102",
+            "ABS": "0x103",
+            "TCM":"0x104",
+            "TPMS": "0x105"
+        }
 
+    def __init__(self):
+        self.messages=[]
+        
     def send_message(self, sender, receiver, message):
+        can_id=self.id_codes.get(receiver.upper(),"0x000")
         can_message = {
+            id:can_id,
             "date":dt.now().strftime("%d/%m/%Y %H:%M:%S"),
             "sender": sender,
             "receiver": receiver,
