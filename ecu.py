fd1ee693d3a25e0c82bee7fa08085a8629071de9
dan28076626 <dan28076626@gmail.com>
2026-07-08 16:57:43 +0000

    Added a start engine function

--- ecu.py
+++ ecu.py
@@ -17,4 +17,15 @@ class ECU:
         else:
             print("Incorrect key, Access denied")
 
+    def start_engine(self):
+        if not self.car.doors_locked and self.car.fuel>0 and self.car.gear=="P":
+            self.car.set_engine(True)
+        elif self.car.doors_locked:
+            print("Cannot start if the door is locked")
+
+        elif self.car.fuel<=0:
+            print("Cannot start if there is no fuel")
+
+        elif self.car.gear!="P":
+            print("Cannot start the car if the car is not in park")
    
